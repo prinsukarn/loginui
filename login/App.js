7df7f28d35ded1bbb6d29752b523723caa4d7170
Pratiksha Karn <pratikshakarn@Pratikshas-MacBook-Pro.local>
2024-07-18 12:50:30 +1000

    feat: add login and signupscreen, add navigation

--- login/App.js
+++ login/App.js
@@ -1,46 +1,55 @@
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from "react-native-reanimated";
-import { View, Button } from "react-native";
+// import Animated, {
+//   useSharedValue,
+//   withTiming,
+//   useAnimatedStyle,
+//   Easing,
+// } from "react-native-reanimated";
+// import { View, Text } from "react-native";
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
+// export default function AnimatedStyleUpdateExample(props) {
+//   const randomWidth = useSharedValue(10);
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
+//   const config = {
+//     duration: 500,
+//     easing: Easing.bezier(0.5, 0.01, 0, 1),
+//   };
 
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
+//   const style = useAnimatedStyle(() => {
+//     return {
+//       width: withTiming(randomWidth.value, config),
+//     };
+//   });
 
+//   return (
+//     <View>
+//       <Text>Hello World!</Text>
+//     </View>
+//   );
+// }
+
+// In App.js in a new project
+
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/loginScreen";
+import SignupScreen from "./screens/signupScreen";
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
