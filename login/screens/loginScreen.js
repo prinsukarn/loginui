f3cb57973de78283fc41e067d5932833cea5d559
Pratiksha Karn <pratikshakarn@Pratikshas-MacBook-Pro.local>
2024-07-19 20:33:09 +1000

    feat: add lights to background, fix errors

--- login/screens/loginScreen.js
+++ login/screens/loginScreen.js
@@ -1,15 +1,26 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StatusBar } from "react-native";
 import React from "react";
 
 export default function LoginScreen() {
   return (
     <View className="bg-white h-full w-full">
+      <StatusBar style="light" />
       <Image
         style={{ resizeMode: "cover", width: "100%", height: "100%" }}
         source={require("../assets/images/background.png")}
         resizeMode={"cover"}
       />
-      <Text>Login</Text>
+      {/* Lights */}
+      <View className="flex-row justify-around w-full absolute">
+        <Image
+          className="h-[225] w-[90]"
+          source={require("../assets/images/light.png")}
+        />
+        <Image
+          className="h-[160] w-[65]"
+          source={require("../assets/images/light.png")}
+        />
+      </View>
     </View>
   );
 }
