d647bdf155427f59a82a4d85154426fe22ef3ede
Pratiksha Karn <pratikshakarn@Pratikshas-MacBook-Pro.local>
2024-07-19 15:16:47 +1000

    feat: add images

--- login/App.js
+++ login/App.js
@@ -1,36 +1,3 @@
-// import Animated, {
-//   useSharedValue,
-//   withTiming,
-//   useAnimatedStyle,
-//   Easing,
-// } from "react-native-reanimated";
-// import { View, Text } from "react-native";
-
-// export default function AnimatedStyleUpdateExample(props) {
-//   const randomWidth = useSharedValue(10);
-
-//   const config = {
-//     duration: 500,
-//     easing: Easing.bezier(0.5, 0.01, 0, 1),
-//   };
-
-//   const style = useAnimatedStyle(() => {
-//     return {
-//       width: withTiming(randomWidth.value, config),
-//     };
-//   });
-
-//   return (
-//     <View>
-//       <Text>Hello World!</Text>
-//     </View>
-//   );
-// }
-
-// In App.js in a new project
-
-import * as React from "react";
-import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/loginScreen";
--- login/screens/loginScreen.js
+++ login/screens/loginScreen.js
@@ -1,9 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import React from "react";
 
 export default function LoginScreen() {
   return (
-    <View>
-      <Text>Login Sister</Text>
+    <View className="bg-white h-full w-full">
+      <Image
+        style={{ resizeMode: "cover", width: "100%", height: "100%" }}
+        source={require("../assets/images/background.png")}
+        resizeMode={"cover"}
+      />
+      <Text>Login</Text>
     </View>
   );
 }
